2b20acce5e75dfec0d551d457d7abaec707572c7
Andrews Odame <odameandy8989@gmail.com>
2023-02-16 21:30:56 +0000

    gcc practice

---
+++ file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("i come from asiakwa in the eastern region\n");
+	printf("i'm still trying to learn coding");
+
+}
---
+++ file.s
@@ -0,0 +1,31 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 13, 0	sdk_version 13, 1
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	stp	x29, x30, [sp, #-16]!           ; 16-byte Folded Spill
+	mov	x29, sp
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	adrp	x0, l_.str.1@PAGE
+	add	x0, x0, l_.str.1@PAGEOFF
+	bl	_printf
+	mov	w0, #0
+	ldp	x29, x30, [sp], #16             ; 16-byte Folded Reload
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"i come from asiakwa in the eastern region\n"
+
+l_.str.1:                               ; @.str.1
+	.asciz	"i'm still trying to learn coding"
+
+.subsections_via_symbols
