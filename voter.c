0d6199b527d28b7ac07ffcfd859e0bea6e6cecb8
Andrews Odame <odameandy8989@gmail.com>
2023-03-07 10:57:27 -0800

    Learning

---
+++ voter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	    int x;
+	        printf("Enter a number: ");
+		    scanf("%d", &x);
+		        
+		        if (x > 17)
+				    printf("Person is eligible to vote\n");
+			    
+			    else
+				        printf("Person is underage\n");
+			        return 0;
+}
